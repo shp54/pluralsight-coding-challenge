626b9d350539eb4d004db22a8a22af893484dcee
Shameek Poddar <shameek.poddar@gmail.com>
2018-05-02 21:15:46 -0400

    Break up data loading and Flask app

--- app.py
+++ app.py
@@ -1,80 +1,35 @@
-
 from flask import Flask, jsonify, request
 import json
-
-CSV_DATA = 'code_challenge_question_dump.csv'
-data = {}
-maxId = 0
-
-def getDataFromFile():
-    global maxId
-    id = 0
-    with open(CSV_DATA) as f:
-        for line in f.readlines():
-            if id == 0:
-                id += 1
-                continue
-            [ question, answer, distractors ] = line.strip().split('|')
-            data[id] = {
-                "id": id,
-                "question": question,
-                "answer": answer,
-                "distractors": distractors.split(',')
-            }
-            id += 1
-    maxId = id
-
+import data
 
 app = Flask(__name__)
 
 @app.route('/questions', methods=['GET'])
 def questions():
-    results = [v for k, v in list(data.items())]
+    results = data.getAllItems()
     return jsonify(results)
 
 @app.route('/questions', methods=['POST'])
 def create_questions():
-    global maxId
     jsonData = json.loads(request.data.decode())
-    data[maxId] = {
-        "id": maxId,
-        "question": jsonData['question'],
-        "answer": jsonData['answer'],
-        "distractors": jsonData['distractors']
-    }
-    newId = maxId
-    maxId += 1
-    return jsonify(data[newId])
+    result = data.createItem(jsonData)
+    return jsonify(result)
 
 @app.route('/questions/<id>', methods=['GET'])
 def get_question(id):
-    key = int(id)
-    if key in data:
-        return jsonify(data[key])
-    else:
-        return jsonify({ 'error': 'Tried to access nonexistent item' })
+    return jsonify(data.getItem(id))
 
 @app.route('/questions/<id>', methods=['PUT'])
 def update_question(id):
-    key = int(id)
     jsonData = json.loads(request.data.decode())
-    if key in data:
-        data[key].update({
-            "question": jsonData['question'],
-            "answer": jsonData['answer'],
-            "distractors": jsonData['distractors'],
-        })
-        return jsonify(data[key])
-    else:
-       return jsonify({ 'error': 'Tried to access nonexistent item' }) 
+    return jsonify(data.updateItem(id, jsonData))
 
 @app.route('/questions/<id>', methods=['DELETE'])
 def delete_question(id):
-    key = int(id)
-    return jsonify(data.pop(key, {}))
+    return jsonify(data.deleteItem(id))
 
 def main():
-    getDataFromFile()
+    data.getDataFromFile()
     app.run()
 
 if __name__ == '__main__':
---
+++ data.py
@@ -0,0 +1,60 @@
+CSV_DATA = 'code_challenge_question_dump.csv'
+data = {}
+maxId = 0
+
+def getDataFromFile():
+    global maxId
+    id = 0
+    with open(CSV_DATA) as f:
+        for line in f.readlines():
+            if id == 0:
+                id += 1
+                continue
+            [ question, answer, distractors ] = line.strip().split('|')
+            data[id] = {
+                "id": id,
+                "question": question,
+                "answer": answer,
+                "distractors": distractors.split(',')
+            }
+            id += 1
+    maxId = id
+
+def getItem(id):
+    key = int(id)
+    if key in data:
+        return data[key]
+    else:
+        return { 'error': 'Tried to access nonexistent item' }
+
+def getAllItems():
+    return [v for k, v in list(data.items())]
+
+def createItem(jsonData):
+    global maxId
+    data[maxId] = {
+        "id": maxId,
+        "question": jsonData['question'],
+        "answer": jsonData['answer'],
+        "distractors": jsonData['distractors']
+    }
+    newId = maxId
+    maxId += 1
+    return data[newId]
+
+def updateItem(id, jsonData):
+    key = int(id)
+    if key in data:
+        data[key].update({
+            "question": jsonData['question'],
+            "answer": jsonData['answer'],
+            "distractors": jsonData['distractors'],
+        })
+        return data[key]
+    else:
+       return { 'error': 'Tried to access nonexistent item' }
+
+def deleteItem(id):
+    key = int(id)
+    return data.pop(key, {})
+
