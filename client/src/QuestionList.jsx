271c9c9c1e15312f4948063f0657fac01c6ad214
Shameek Poddar <shameek.poddar@gmail.com>
2018-05-05 12:52:05 -0400

    Add ability to create questions (needs refactor)

--- client/src/QuestionList.jsx
+++ client/src/QuestionList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { List } from 'semantic-ui-react';
 import Question from './Question';
+import CreateQuestion from './CreateQuestion';
 import './index.css';
 
 const QuestionList = (props) => (
     <List className='questionList'>
+        <CreateQuestion />
         {props.questions.map(item => <Question question={item} key={item.id} />)}
     </List>
 );
